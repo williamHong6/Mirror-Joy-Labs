f1244435d46a11f09353455633be49e39ab01437
WilliamHong <h.leiyi@wustl.edu>
2025-05-12 22:08:51 -0500

    修改首页跳转逻辑

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import TryOnAI from './pages/TryOnAI.jsx';
 import SkinCapture from "./pages/SkinCapture.jsx";  
@@ -7,11 +7,14 @@ import SkinLoading from './pages/SkinLoading.jsx';
 import SkinResult from './pages/SkinResult.jsx';
 import ProductRecommend from './pages/ProductRecommend.jsx';
 
-
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      {/* 自动从 / 跳转到 /home */}
+      <Route path="/" element={<Navigate to="/home" replace />} />
+
+      {/* 显式的 home 页面 */}
+      <Route path="/home" element={<Home />} />
       <Route path="/skin-ai" element={<SkinCapture />} />
       <Route path="/skin-loading" element={<SkinLoading />} />
       <Route path="/skin-result" element={<SkinResult />} />
