5e9e11abb6cc7b240144ce613207e210735fafce
WilliamHong <h.leiyi@wustl.edu>
2025-05-12 21:44:33 -0500

    base name in main changed

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Mirror-Joy-Labs/">
     <App />
   </BrowserRouter>
 );
